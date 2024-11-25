9caefe9766ee74a3f4f914980df99b425c31a3e9
kjunh972 <kjunh972@gmail.com>
2024-11-25 20:45:13 +0900

    줄바꿈 수정

--- QRManager/src/main/java/com/kjunh972/QRManager/model/QuizScore.java
+++ QRManager/src/main/java/com/kjunh972/QRManager/model/QuizScore.java
@@ -11,37 +11,44 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "quiz_scores")
 public class QuizScore {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String nickname;
-    private int score;
-    private LocalDateTime createdAt;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nickname;
+	private int score;
+	private LocalDateTime createdAt;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNickname() {
 		return nickname;
 	}
+
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+
 	public int getScore() {
 		return score;
 	}
+
 	public void setScore(int score) {
 		this.score = score;
 	}
+
 	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
+
 	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
-    
-    
+
 }
